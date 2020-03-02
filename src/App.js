5a99a009a8406b32884fbdb60fb6738a0c51d950
Tim Hobson <timhobson85@gmail.com>
2020-03-02 13:06:58 +1100

    ghpages added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
-export default App;
+  render(){
+    return(
+      <div className='App'>
+        <h1 id="nameHeader" className="text-flicker-in-glow">tim hobson</h1>
+        <p className="text-appear">under construction</p>
+      </div>
+    )
+  }
+
+} // Class
+
+export default App
